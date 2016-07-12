193eba4756b714a6fad7c82ff345210630bc1573
micheledellameax <tsid11@tecnicosuperiorekennedy.it>
2016-07-12 15:22:44 +0100

    insert comments

--- mapper.x.py
+++ mapper.x.py
@@ -2,19 +2,27 @@
 
 import sys
 
-
+#setting up empty lists
 time = []
 idMach = []
 
+# input comes from STDIN (standard input)
 for line in sys.stdin:
+	#every line become a string
     rowString = str(line)
+    #split lines by ";"
     rowSplit = rowString.split(";")
+    #add elements to list with id
     idMach.append(rowSplit[2])
     timeModify = rowSplit[3]
+    #delete the last character of the time
     timeNew = timeModify[:-1]
+    #add elements to list with time and converting to int
     time.append(int(timeNew))
 
+#print to STDOUT
 for i in range (0, len(time)):
     timePrint = time[i]
     idPrint = idMach[i]
+    #print keys and values (mapping)
     print('%s\t%s' % (idPrint, timePrint))
--- reducer.x.py
+++ reducer.x.py
@@ -3,15 +3,20 @@
 from collections import defaultdict
 import sys
 
-
+#setting up empty default dictionary
 diction = defaultdict(int)
 
+# input comes from STDIN (standard input)
 for line in sys.stdin:
+	#remove whitespaces
 	line = line.strip()
 	idMach, time = line.split('\t')
+	#reducing giving the total value for every key
 	diction[idMach] += int(time)
 
+#reordering default dictionary
 sortedList = sorted(diction.items())
 
 for i in range(0, len(sortedList)):
+	#printing to STDOUT the final result
 	print('%s\t%s' % (sortedList[i][0], sortedList[i][1]))
